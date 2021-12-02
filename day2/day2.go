548d2747033fba84beac962f429a48d7f0d9ee8a
r333mo <65601883+r333mo@users.noreply.github.com>
2021-12-02 15:14:29 +0000

    day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -14,18 +14,28 @@ func main() {
 	var depth int = 0
 
 	// testData := []string{"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"}
+	
 	input, _ := readLines("input.txt")
-
 	for i := range input {
-		foo(input[i], &x, &depth)
-		printCoords(&x, &depth)
+		part1(input[i], &x, &depth)
+		// printCoords(&x, &depth)
 	}
-
-	result := x * depth
-	fmt.Println("Result: ", result)
+	result1 := x * depth
+	
+	x = 0
+	depth = 0
+	var aim int = 0
+	for i := range input {
+		part2(input[i], &x, &depth, &aim)
+		// printCoords(&x, &depth)
+	}
+	result2 := x * depth
+	
+	fmt.Println("Part 1 result: ", result1)
+	fmt.Println("Part 2 result: ", result2)
 }
 
-func foo(raw string, x *int, depth *int) {
+func part1(raw string, x *int, depth *int) {
 	if strings.Contains(raw, "forward") {
 		value, _ := strconv.Atoi(raw[8:])
 		*x += value
@@ -40,6 +50,25 @@ func foo(raw string, x *int, depth *int) {
 		value, _ := strconv.Atoi(raw[5:])
 		*depth += value
 		fmt.Println("Moved", value, "spaces down!")
+	}
+}
+
+func part2(raw string, x *int, depth *int, aim *int) {
+	if strings.Contains(raw, "forward") {
+		value, _ := strconv.Atoi(raw[8:])
+		*x += value
+		*depth += (value * *aim)
+		fmt.Println("Moved", value, "spaces forward!")
+		
+	} else if strings.Contains(raw, "up") {
+		value, _ := strconv.Atoi(raw[3:])
+		*aim -= value
+		fmt.Println("Aimed", value, " up!")
+
+	} else if strings.Contains(raw, "down") {
+		value, _ := strconv.Atoi(raw[5:])
+		*aim += value
+		fmt.Println("Aimed", value, " down!")
 	}
 }
 
