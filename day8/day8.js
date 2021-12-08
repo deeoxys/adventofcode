eecc9d0a383bd8f75ee4c079a08f7a8b7d129b4d
r333mo <65601883+r333mo@users.noreply.github.com>
2021-12-08 10:44:01 +0000

    cleanup

--- day8/day8.js
+++ day8/day8.js
@@ -1,40 +1,14 @@
-// console.log("advent of code day 8");
-
-const one = 2; // 1 uses two digits
-const four = 4; // 4 uses four digits
-const seven = 3; // 7 uses three digits
-const eight = 7; // 8 uses seven digits
-
-// let test_data = "fgae cfgab fg bagce";
-
-const test_data = [
-    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
-    "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
-    "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
-    "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
-    "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
-    "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
-    "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
-    "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
-    "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
-    "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
-];
-
 const data = getData();
-console.log("Test data result: " + fooo(test_data));
-console.log("Actual data result: " + fooo(data));
+console.log(fooo(data));
 
 function getData() {
     var fs = require("fs");
     let text = fs.readFileSync("input.txt", "utf-8");
-    const lines = text.split("\n");
-    return lines;
+    return text.split("\n");
 }
 
 function foo(s) {
-    if (s.length == one || s.length == four || s.length == seven || s.length == eight) {
-        return true;
-    }
+    if (s.length == 2 || s.length == 4 || s.length == 3 || s.length == 7) return true;
     return false;
 }
 
