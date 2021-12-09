7c154a64750ef4e03b1157b841e79fbd577ef623
r333mo <65601883+r333mo@users.noreply.github.com>
2021-12-09 23:39:08 +0000

    MORE

--- day8/day8.js
+++ day8/day8.js
@@ -1,23 +1,13 @@
 // Advent of Code: Day 8 📜
-function getData() {
-    return require("fs").readFileSync("input.txt", "utf-8").split("\n");
-}
-
-function foo(s) {
-    return (s.length == 2 || s.length == 4 || s.length == 3 || s.length == 7);
-}
-
-function fooo(data) {
+function foo(data) {
     let total = 0;
     for (let i = 0; i < data.length; i++) {
-        let s = data[i].substr(data[i].indexOf("|") + 2);
-        const arr = s.split(" ");    
+        const arr = data[i].substr(data[i].indexOf("|") + 2).split(" ");    
         for (let j = 0; j < arr.length; j++)
-            if (foo(arr[j]) === true)
+            if (arr[j].length == 2 || arr[j].length == 4 || arr[j].length == 3 || arr[j].length == 7)
                 total += 1;
     }
     return total;
 }
 
-const data = getData();
-console.log("Part 1 result: " + fooo(data));
+console.log("Part 1 result: " + foo(require("fs").readFileSync("input.txt", "utf-8").split("\n")));
