76abcb7316b8e4cc2b7e6e599980f81ea84c9024
r333mo <65601883+r333mo@users.noreply.github.com>
2021-12-09 19:37:32 +0000

    day 4 cleanup

--- day4/day4.java
+++ day4/day4.java
@@ -1,3 +1,4 @@
+// Advent of Code: Day 4 ☕
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,11 +14,9 @@ public class day4 {
         ArrayList<String> numbersToCall = new ArrayList<>();
         ArrayList<Player> players = new ArrayList<>();
         ArrayList<String> lines = new ArrayList<>();
-
         try {
             File f = new File("input.txt");
             Scanner sc = new Scanner(f);
-
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
                 lines.add(line);
@@ -26,50 +25,34 @@ public class day4 {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         // get numbers to call
         var vals = Arrays.stream(lines.get(0).split(","))   // new java 🤔
             .collect(Collectors.toList());
-        for (var val : vals) {
-            numbersToCall.add(val);
-        }
-        
-        
-        // get bingo stamp cards
-        // get rid of numbers to call and blank line
+        for (var val : vals) numbersToCall.add(val);
         lines.remove(0);
         lines.remove(0);
-
         int cardNumber = 1;
-        
         for (int line = 0; line < lines.size(); line++) {
             if (lines.get(line).equals("")) {
-                // System.out.println("detected bingo card");
                 cardNumber++;
                 String[][] values = new String[5][5];
                 int localCardRow = 0;
                 for (int cardRow = line + 1; cardRow <= line + 5; cardRow++) {
                     String cardRowString = lines.get(cardRow);
-                    // System.out.println("\nCard row " + cardRow + ": " + lines.get(cardRow));
-                    
-                    Pattern pattern = Pattern.compile("(^..) (..) (..) (..) (..$)");
+                    Pattern pattern = Pattern.compile("(^..) (..) (..) (..) (..$)");    // regex pattern for row of numbers here
                     Matcher matcher = pattern.matcher(cardRowString);
                     if (matcher.find()) {
                         String[] rowValues = new String[5];
-                        for (int groupNumber = 1; groupNumber <= matcher.groupCount(); groupNumber++) {
+                        for (int groupNumber = 1; groupNumber <= matcher.groupCount(); groupNumber++)
                             rowValues[groupNumber - 1] = matcher.group(groupNumber);
-                        }
                         values[localCardRow] = rowValues;
                         localCardRow++;
                     }
                 }
                 Player player = new Player(cardNumber, values);
                 players.add(player);
-                System.out.println("\n" + player + "\n");
             }
         }
-        
-        
         Game game = new Game(numbersToCall, new ArrayList<Player>(players));
         game.play();
     }
@@ -87,22 +70,17 @@ class Game {
     public String drawNumber() {
         String calledNumber = numbersToCall.get(0);
         numbersToCall.remove(0);
-        
         return calledNumber;
     }
 
     public void updatePlayers(String number) {
-        for (Player player : players) {
-            player.update(number);
-        }
+        for (Player player : players) player.update(number);
     }
 
     public Map<Player, Integer> checkForWinConditions() {
         for (Player player : players) {
             Map<Player, Integer> map = player.checkForWinCondition();
-            if (map != null) {
-                return map;
-            }
+            if (map != null) return map;
         }
         return null;
     }
@@ -110,18 +88,10 @@ class Game {
     public void play() {
         for (int i = numbersToCall.size() - 1; i >= 0; i--) {
             String calledNumber = drawNumber();
-            System.out.println("The called number is " + calledNumber + "!");
-            
             updatePlayers(calledNumber);
-            
             Map<Player, Integer> map = checkForWinConditions();
-
             if (map != null) {
-                System.out.println("\n" + map.keySet().toArray()[0]);
-                System.out.println("Player " + ((Player) map.keySet().toArray()[0]).getId() + " wins!");
-                System.out.println("The sum of the unmarked numbers was: " + map.get(map.keySet().toArray()[0]));
-                System.out.println("The sum of the unmarked numbers multiplied by the winning number was: " + ((int) (map.get(map.keySet().toArray()[0])) * Integer.parseInt(calledNumber)));
-
+                System.out.println("Part 1 result: " + ((int) (map.get(map.keySet().toArray()[0])) * Integer.parseInt(calledNumber)));
                 return;
             }
         }
@@ -141,79 +111,42 @@ class Player {
         return this.id;
     }
 
-    @Override
-    public String toString() {
-        String s = "Player " + id + "\n";
-        
-        for (int row = 0; row < 5; row++) {
-            for (int column = 0; column < 5; column++) {
-                boolean cross = false;
-                if (stampSheet[row][column].equals("XX")) cross = true;
-                if (cross) s += "\u001B[31m";
-                s += stampSheet[row][column];
-                if (cross) s += "\u001B[0m";
-                if (column < 4) s += " ";
-            }
-            s += "\n";
-        }
-        
-        return s;
-    }
-
     public void update(String number) {
-        for (int row = 0; row < 5; row++) {
-            for (int column = 0; column < 5; column++) {
-                if (stampSheet[row][column].trim().equals(number)) {
+        for (int row = 0; row < 5; row++)
+            for (int column = 0; column < 5; column++)
+                if (stampSheet[row][column].trim().equals(number))
                     stampSheet[row][column] = "XX";
-                }
-            }
-        }
     }
 
     public Map<Player, Integer> checkForWinCondition() {
         Map<Player, Integer> map = new HashMap<Player, Integer>();
-        
         boolean win = false;
         int sum = 0;
         int streak = 0;
-
-        // check for horizontal win
-        for (int row = 0; row < 5; row++) {
+        for (int row = 0; row < 5; row++) { // check for horizontal win condition
             for (int column = 0; column < 5; column++) {
-                if (stampSheet[row][column].equals("XX")) {
-                    streak++;
-                } else {
-                    sum += Integer.parseInt(stampSheet[row][column].trim());
-                }
+                if (stampSheet[row][column].equals("XX")) streak++;
+                else sum += Integer.parseInt(stampSheet[row][column].trim());
                 if (streak == 5) win = true;
             }
             streak = 0;
         }
-        
         if (win) {
-            System.out.println("Unmarked numbers sum: " + sum);
             map.put(this, sum);
             return map;
         }
-
-        // check for vertical win
-        for (int column = 0; column < 5; column++) {
+        for (int column = 0; column < 5; column++) {    // check for vertical win condition
             for (int row = 0; row < 5; row++) {
-                if (stampSheet[row][column].equals("XX")) {
-                    streak++;
-                } else {
-                    sum += Integer.parseInt(stampSheet[row][column].trim());
-                }
+                if (stampSheet[row][column].equals("XX")) streak++;
+                else sum += Integer.parseInt(stampSheet[row][column].trim());
                 if (streak == 5) win = true;
             }
             streak = 0;
         }
         if (win) {
-            System.out.println("Unmarked numbers sum: " + sum);
             map.put(this, sum);
             return map;
         }
-
         return null;
     }
 }
