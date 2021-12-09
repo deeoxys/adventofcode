41f505bbb71fb101ebf59518c63deb8af912966a
r333mo <65601883+r333mo@users.noreply.github.com>
2021-12-09 19:22:32 +0000

    day 3 cleanup

--- day3/day3.rs
+++ day3/day3.rs
@@ -1,6 +1,4 @@
-// First program in rust. Not proud at all.
-// struggled a fair amount with this lang, an experience in learning..
-
+// Advent of Code: Day 3 🦀
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
@@ -8,64 +6,39 @@ use std::{
 };
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("no such file");
+    let file = File::open(filename).expect("File not found");
     let buf = BufReader::new(file);
     buf.lines()
         .map(|l| l.expect("Could not parse line"))
         .collect()
 }
 
-fn print_type_of<T: ?Sized>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
-
 fn main() {
-    println!("Advent of Code Day 3");
-
     let p: &str = "input.txt";
     let test_data = lines_from_file(&p);
-
-    // STAGE 2: put into vector
     let mut data: Vec<Vec<char>> = vec![vec!['0'; test_data.len()]; 12];
-    println!("Printing 2d vector");
-    for vec1d in data.iter() {
-        println!("{:?}", vec1d);
-    }
-    println!("");
-    
     let mut char_index = 0;
     let mut row = 0;
     let x: usize = 12;
-
     for str in test_data.iter() {
         let char_vec: Vec<char> = str.chars().collect();
-
         for _chr in char_vec.iter() {
             data[char_index][row] = char_vec[char_index];
-            
             if char_index < x {
                 char_index += 1;
-            } // rust compiler moment
+            }
+            // rust compiler moment
             if char_index >= x {
                 char_index = 0;
             }
         }
         row += 1;
     }
-
-    println!("\nPrinting 2d vector");
-    for vec1d in data.iter() {
-        println!("{:?}", vec1d);
-    }
-
-    // STAGE 3: determine if there are more ones or zeros for each vector
     let mut more_ones_than_zeros = vec![false; 12];
-    
     let mut ones = 0;
     let mut zeros = 0;
     row = 0;
     for vec1d in data.iter() {
-        // println!("row: {}", row);
         for column in vec1d.iter() {
             if *column == '1' {
                 ones += 1;
@@ -80,35 +53,22 @@ fn main() {
         zeros = 0;
         row += 1;
     }
-
-    println!("\nmore_ones_than_zeros: {:?}\n", more_ones_than_zeros);
-
     let mut gamma_rate = [0; 12];
     let mut epsilon_rate = [0; 12];
-
     for i in 0..more_ones_than_zeros.len() {
-        println!("i: {}, value: {}", i, more_ones_than_zeros[i]);
         if more_ones_than_zeros[i] {
             gamma_rate[i] = 1;
         } else {
             epsilon_rate[i] = 1;
         }
     }
-    println!("\ngamma_rate: {:?}", gamma_rate);
-    println!("epsilon_rate: {:?}", epsilon_rate);
-
     let decimal_gamma_rate = bin_to_dec(&gamma_rate);
     let decimal_epsilon_rate = bin_to_dec(&epsilon_rate);
-    
-    println!("\nDecimal gamma_rate: {}", decimal_gamma_rate);
-    println!("Decimal epsilon_rate: {}", decimal_epsilon_rate);
-
-    println!("\nDecimal gamma_rate multiplied by decimal epsilon_rate: {}", decimal_gamma_rate * decimal_epsilon_rate);
+    println!("Part 1 result: {}", decimal_gamma_rate * decimal_epsilon_rate);
 }
 
 fn bin_to_dec(v: &[i32]) -> i32 {
     let mut decimal = 0;
-    
     let mut column = 0;
     for binary in v.iter() {
         if *binary == 1 {
@@ -125,7 +85,7 @@ fn bin_to_dec(v: &[i32]) -> i32 {
                 9 => decimal += 4,
                 10 => decimal += 2,
                 11 => decimal += 1,
-                _ => println!("Error"),
+                _ => println!("Error parsing bit"),
             }
         }
         column += 1;
