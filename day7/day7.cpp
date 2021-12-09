901e036251545008da066b8810f8f8c43ca88008
r333mo <65601883+r333mo@users.noreply.github.com>
2021-12-09 19:58:50 +0000

    day 7 cleanup

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -1,14 +1,13 @@
+// Advent of Code: Day 7 🇨➕➕
 #include <bits/stdc++.h>
 using namespace std;
 
 int mostFrequent(int arr[], int size) {
     sort(arr, arr + size);
-
     int max_count = 1, res = arr[0], curr_count = 1;
     for (int i = 1; i < size; i++) {
-        if (arr[i] == arr[i - 1]) {
-            curr_count++;
-        } else {
+        if (arr[i] == arr[i - 1]) curr_count++;
+        else {
             if (curr_count > max_count) {
                 max_count = curr_count;
                 res = arr[i - 1];
@@ -25,52 +24,31 @@ int mostFrequent(int arr[], int size) {
 
 int foo(int arr[], int n, int size) {
     int fuel_used = 0;
-
     for (int i = 0; i < size; i++) {
-        if (arr[i] > n) {
-            fuel_used += arr[i] - n;
-        } else {
-            fuel_used += n - arr[i];
-        }
+        if (arr[i] > n) fuel_used += arr[i] - n;
+        else fuel_used += n - arr[i];
     }
-
     return fuel_used;
 }
 
 int main() {
-    // int test_data[] = {16, 1, 2, 0, 4, 2, 7, 1, 2, 14};
-    // int size = sizeof(test_data) / sizeof(test_data[0]);
-
-    int data[1000]; // hard coded. meh
-
+    int data[1000];
     int c = 0;
     string line;
     ifstream myfile("input.txt");
     if (myfile.is_open()) {
         while (!myfile.eof()) {
             getline(myfile, line, ',');
-            // cout << line << endl;
             data[c] = stoi(line);
-            c++;
+            c++;    // 😊
         }
         myfile.close();
     }
-
-    // find lowest
     int fuel_used = 0;
     for (int i = 1; i <= 1000; i++) {
-        if (i == 1) {
-            fuel_used = foo(data, 1, 1000);
-        }
-        
-        // cout << foo(data, i, 1000) << endl;
-        if (foo(data, i, 1000) < fuel_used) {
-            fuel_used = foo(data, i, 1000);
-        }
+        if (i == 1) fuel_used = foo(data, 1, 1000);
+        if (foo(data, i, 1000) < fuel_used) fuel_used = foo(data, i, 1000);
     }
-
-    // fuel_used = foo(data, mostFrequent(data, 1000), 1000);
-    cout << "Fuel used in best case scenario: " << fuel_used << endl;
-    
-    return 0;
+    cout << "Part 1 result: " << fuel_used << endl;
+    return 1337;
 }
