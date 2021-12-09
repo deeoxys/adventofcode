e4cb7c0511b6a2f2aa8de9f88ca752246dd3f7b9
r333mo <65601883+r333mo@users.noreply.github.com>
2021-12-09 19:16:15 +0000

    day 2 cleanup

--- day2/day2.go
+++ day2/day2.go
@@ -1,4 +1,4 @@
-// first program in go, lets se how it *go*es :)
+// Advent of Code: Day 2
 package main
 
 import (
@@ -12,41 +12,47 @@ import (
 func main() {
 	var x int = 0
 	var depth int = 0
-	// testData := []string{"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"}
-
+	test_data := []string{"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"}
+	for i := range test_data {
+		part1(test_data[i], &x, &depth)
+	}
+	fmt.Println("Part 1 result (Test data):", (x * depth))
+	
+	x = 0
+	depth = 0
 	input, _ := readLines("input.txt")
 	for i := range input {
 		part1(input[i], &x, &depth)
 	}
-	result1 := x * depth
-
+	fmt.Println("Part 1 result:", (x * depth))
+	
 	x = 0
 	depth = 0
 	var aim int = 0
+	for i := range test_data {
+		part2(test_data[i], &x, &depth, &aim)
+	}
+	fmt.Println("Part 2 result (Test data):", (x * depth))
+	
+	x = 0
+	depth = 0
+	aim = 0
 	for i := range input {
 		part2(input[i], &x, &depth, &aim)
 	}
-	result2 := x * depth
-
-	fmt.Println("Part 1 result: ", result1)
-	fmt.Println("Part 2 result: ", result2)
+	fmt.Println("Part 2 result:", (x * depth))
 }
 
 func part1(raw string, x *int, depth *int) {
 	if strings.Contains(raw, "forward") {
 		value, _ := strconv.Atoi(raw[8:])
 		*x += value
-		fmt.Println("Moved", value, "spaces forward!")
-
 	} else if strings.Contains(raw, "up") {
 		value, _ := strconv.Atoi(raw[3:])
 		*depth -= value
-		fmt.Println("Moved", value, "spaces up!")
-
 	} else if strings.Contains(raw, "down") {
 		value, _ := strconv.Atoi(raw[5:])
 		*depth += value
-		fmt.Println("Moved", value, "spaces down!")
 	}
 }
 
@@ -55,31 +61,18 @@ func part2(raw string, x *int, depth *int, aim *int) {
 		value, _ := strconv.Atoi(raw[8:])
 		*x += value
 		*depth += (value * *aim)
-		fmt.Println("Moved", value, "spaces forward!")
-
 	} else if strings.Contains(raw, "up") {
 		value, _ := strconv.Atoi(raw[3:])
 		*aim -= value
-		fmt.Println("Aimed", value, "up!")
-
 	} else if strings.Contains(raw, "down") {
 		value, _ := strconv.Atoi(raw[5:])
 		*aim += value
-		fmt.Println("Aimed", value, "down!")
 	}
 }
 
-func printCoords(x *int, y *int) {
-	fmt.Println("Coords:", *x, *y)
-}
-
 func readLines(path string) ([]string, error) {
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
+    file, _ := os.Open(path)
     defer file.Close()
-
     var lines []string
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
