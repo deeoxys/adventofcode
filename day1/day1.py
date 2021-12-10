f5cfcbe6913302199d3f8a99495409e52ca1e973
r333mo <65601883+r333mo@users.noreply.github.com>
2021-12-10 00:15:50 +0000

    MORE

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,4 @@
 # Advent of Code 2021: Day 1 🐍
-test_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-input = []
-with open("input.txt") as f:
-    for line in f:
-        stripped = line.strip()
-        input.append(int(stripped))
-    f.close()
-
 def part1(list):
     last = -1
     results = []
@@ -21,13 +13,6 @@ def part1(list):
                 results.append("decreased")
             last = list[i]
     return results
-
-def count(list):
-    count = 0
-    for i in range(len(list)):
-        if list[i] == "increased":
-            count += 1
-    return count
 
 def part2(list):
     last = -1
@@ -48,7 +33,12 @@ def part2(list):
                 last = total
     return results
 
-print("Part 1 (Test Data): " + str(count(part1(test_data))))
-print("Part 2 (Test Data): " + str(count(part2(test_data))))
-print("Part 1 :" + str(count(part1(input))))
-print("Part 2 :" + str(count(part2(input))))
+test_data, input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263], 
+with open("input.txt") as f:
+    for line in f:
+        input.append(int(line.strip()))
+    f.close()
+print("Part 1 (Test Data): " + str(sum("increased" in s for s in part1(test_data))))
+print("Part 2 (Test Data): " + str(sum("increased" in s for s in part2(test_data))))
+print("Part 1 :" + str(sum("increased" in s for s in part1(input))))
+print("Part 2 :" + str(sum("increased" in s for s in part2(input))))
